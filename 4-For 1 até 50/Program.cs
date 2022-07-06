18316bcc9608b531aa30464ea9d4067fbce3c63c
cesar <cesar@DESKTOP-0S8NU6M>
2022-07-06 18:20:04 -0300

    Adicionar arquivos de projeto.

---
+++ 4-For 1 até 50/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_For_1_até_50
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Contador do 1 ao 50");
+
+            
+
+            for (int num1 = 1;num1 <=50;  num1++)
+            {
+                Console.WriteLine("\t" + num1);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
